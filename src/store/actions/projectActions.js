7caa4736e725554b39cdbe8ef8c4c73a0c428f61
joeyy-watts <joeyms40@gmail.com>
2020-12-10 12:15:51 +0700

    Updated files in store directory.

--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -1,6 +1,19 @@
 export const createProject = (project) => {
-    return(dispatch, getState) => {
-        dispatch({ type:"CREATE_RESTAURANT", project});
+    return(dispatch, getState, { gerFirebase, getFirestore}) => {
+        const firestore = getFirestore();
+        firestore.collection('Restaurants').add({
+            ...project,
+            authorFirstName: 'net',
+            authorLastName: 'lastnet',
+            authorID: 12345,
+            createdAt: new Date()
+
+        }).then(() => {
+            dispatch({ type:"CREATE_RESTAURANT", project});
+        }).catch((err)=>{
+            dispatch({ type: "CREATE_RESTAURANT_ERROR", err });
+        })
+        
     }
 
 };
